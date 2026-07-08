1683daa9508174da91773cbe2e1c5b2b85016e8d
Vincent Loretta <vincentloretta@Vincents-MacBook-Air.local>
2026-07-07 21:54:28 -0400

    fix esm imports and tsconfig for production build

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '../generated/prisma/client';
+// @ts-ignore
+import { PrismaClient } from '../generated/prisma/client/index.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
